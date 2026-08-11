ffed6f5fda8324b77f43493547e3770ae005d373
spokedu77-ops <spokedu77@gmail.com>
2026-08-12 01:46:30 +0900

    prevent blank home media fallbacks

--- app/spokedu/components/visual/media-renderer.tsx
+++ app/spokedu/components/visual/media-renderer.tsx
@@ -59,9 +59,6 @@ export function MediaRenderer({
   };
 
   if (!useImage || !imgSrc) {
-    if (strictPhoto) {
-      return null;
-    }
     return (
       <GradientVisual
         media={media}
